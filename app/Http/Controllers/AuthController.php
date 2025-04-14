1fbf2e1192487794c86780e545979233466b9f00
MRaditFebriansyah <muhammadradit2021@gmail.com>
2025-04-14 15:57:26 +0700

    update kesalahan 5

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,6 +26,7 @@ class AuthController extends Controller
             'username' => 'required|string|max:255|unique:users,username',
             'email' => 'required|string|email|max:255|unique:users,email',
             'password' => 'required|string|min:6|confirmed',
+            
         ]);
 
         User::create([
